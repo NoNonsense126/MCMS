f74f809fe7cab179ab0285d517565c5464b1c2f4
Wong You Jing <wongyoujing118@gmail.com>
2016-01-19 14:16:09 -0800

    Initial Commit

---
+++ MCMS/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MCMS
+//
+//  Created by Wong You Jing on 19/01/2016.
+//  Copyright © 2016 NoNonsense. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
